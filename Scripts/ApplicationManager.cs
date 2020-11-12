df052581e52d452c7e681acba6f13a50cf0d6cc0
Andrew Owen <47571927+Andr3w0w3n@users.noreply.github.com>
2020-11-12 13:39:17 -0700

    ScriptsUpdate

---
+++ Scripts/ApplicationManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ApplicationManager : MonoBehaviour
+{
+    public string defaultDirectory = @"C:\Users\epica\OneDrive\Documents";
+    public static ApplicationManager instance;
+    //assign the instance as "this" if none currently exist
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        //May uncomment this when I figure out how to make this application ready for VR
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/NewFolder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Runtime.InteropServices;
+//using System.Diagnostics;
+
+public class NewFolder : MonoBehaviour
+{
+    private string defaultDir; //this will be the directory that will be defaulted from the start screen
+    private Boolean onFolder;
+    //public GameObject fileFolder;
+    //private Boolean mouseOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultDir = ApplicationManager.instance.defaultDirectory;
+        onFolder = false;
+    }
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }*/
+
+    void OnMouseOver()
+    {
+        onFolder = true;
+    }
+    void OnMouseExit() 
+    { 
+        onFolder = false;
+    }
+    void OnMouseDown()
+    {
+        if(onFolder == true)
+        {
+            //puts all file names into a string array
+            string[] files = Directory.GetFiles(defaultDir);
+            string[] directories = Directory.GetDirectories(defaultDir);
+            for (int i = 0; i < directories.Length - 1; i++)
+            {
+                string result;
+                result = Path.GetFileName(directories[i]);
+                Debug.Log(result);
+            }
+            for (int i = 0; i < files.Length - 1; i++)
+            {
+                string result;
+                result = Path.GetFileName(files[i]);
+                Debug.Log(result);
+            }
+        }
+    }
+}
